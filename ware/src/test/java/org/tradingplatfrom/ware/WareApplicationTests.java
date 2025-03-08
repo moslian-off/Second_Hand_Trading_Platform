f9a0c29a9cc3b804c55b42178c1b9a5896a68da1
mostima <wwlzju@163.com>
2025-03-09 01:35:58 +0800

    First commit. Build a basic structure for the whole project. Divide into 5 microservices.

---
+++ ware/src/test/java/org/tradingplatfrom/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradingplatfrom.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
