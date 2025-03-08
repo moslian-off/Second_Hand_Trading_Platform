11b8dd659f5d1178f914b2cab7494ddeb7a6c9fe
mostima <wwlzju@163.com>
2025-03-09 01:16:16 +0800

    First commit. Build a basic structure for the whole project. Divide into 5 microservices.

---
+++ products/src/main/java/org/tradingplatfrom/products/Controller/ProductController.java
@@ -0,0 +1,7 @@
+package org.tradingplatfrom.products.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+}
---
+++ products/src/main/java/org/tradingplatfrom/products/Service/ProductsService.java
@@ -0,0 +1,8 @@
+package org.tradingplatfrom.products.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductsService {
+
+}
